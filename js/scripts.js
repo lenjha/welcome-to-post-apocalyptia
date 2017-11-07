e8e6caa90b636a8917cf8ca1f06aa83ff778c224
Chris Quattrocchi and Lena Ha <chris+lena@>
2017-11-07 14:19:36 -0800

    examine tube 1 enables take PIPE, pipe enables smash tube2, smash tube2 enables corpse examine, corpse examine enables keycard take

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,28 @@
 //////OBJECTS for examining. ITEM INDEX MUST MATCH DESCRIPTION INDEX!!!
 var ObjExamine = {
   "items": [
-    "CRYOTUBE", //item 1
-     "CORPSE"], //item 2
+
+    // "CRYOTUBE1", // viewobj1
+
+    "CRYOTUBE2", // viewobj2
+
+    // "CORPSE", //viewobj3
+
+    "SCANNER", //viewobj4
+
+    "DOOR"], //viewobj5
 
   "description":[
-    "The cryotube looks as though it is filled with blue raspberry Jell-O. It looks like it's falling apart, and the guy inside looks none too happy. You notice a loose pipe that you might be able to pry off", //desc 1
-     "He's dead, but he may still be useful to you. Could he be carrying something?"] //desc 2
+
+    // "The cryotube looks as though it is filled with blue raspberry Jell-O. It looks like it's falling apart, and the guy inside looks none too happy. You notice a loose PIPE that you might be able to pry off", //desc 1
+
+    "This is a cryotube, but a different one. Flavvvvvvor-text!", //desc 2
+
+    // "He's dead, but he may still be useful to you. Could that be a KEYCARD sticking out of his pocket?", //desc 3
+
+    "A typical g-34t keycard SCANNER. Useless to you, unless of course you have a KEYCARD..", //desc 4
+
+    "A door. Somewhat rusted, but you'll never get through without a KEYCARD"] // desc 5
 };
 
 
@@ -15,8 +31,10 @@ var ObjExamine = {
 //////LIST OF ARRAYS
 var inventoryArray = [];
 var useArray = ["DOOR", "BUTTON"]; //interaction objects
-var examineArray = ["CRYOTUBE", "FLOOR"];    //array for reference only. these can be described
-var takeArray = ["SCREWDRIVER", "TAPE", "DOGFOOD"]; //these can be removed from enviro
+var examineArray = ["CRYOTUBE1", "CRYOTUBE2", "CORPSE", "SCANNER", "DOOR"];    //array for reference only. these can be DESCRIBED with EXAMINE
+
+var takeArray = []; //these can be removed from takeArray and placed in inventoryArray
+                    //objects to be added via examine: PIPE, KEYCARD
 
 /////CHANGES SCENE
 var unlock = function(useInput) {
@@ -25,13 +43,15 @@ var unlock = function(useInput) {
   }
 }
 
-
+var tubeSmashed = false;
 
 ////LIST OF FUNCTIONS that empower USER ACTIONS
-var useFeature = function(useInput) {
+var useFeature = function(useInput) {     ///USE STUFF
   for (i = 0; i < useArray.length; i++) {
-    if (useInput === "DOOR" && !(inventoryArray.includes("SCREWDRIVER"))) {
-      alert("The door is locked shut.  Looks like it can be jimmied open though.");
+    if (useInput === "CRYOTUBE2" && (inventoryArray.includes("PIPE"))) {
+      tubeSmashed = true;
+      return "You smash open the tube, revealing the CORPSE within";
+
     }
     if (useArray[i] === useInput) {
       return "YOU USED SOMETHING";
@@ -40,21 +60,27 @@ var useFeature = function(useInput) {
   return "you used nothing";
 }//end examineFeature function
 
-var examineFeature = function(examineInput) {
+var examineFeature = function(examineInput) {   ///VIEW STUFF
   for (i = 0; i < ObjExamine.items.length; i++) {
-    if (ObjExamine.items[i] === examineInput) {
+    if ((examineInput === "CRYOTUBE1") && !(takeArray.includes("PIPE"))) {
+      takeArray.push("PIPE");
+      return "The cryotube looks as though it is filled with blue raspberry Jell-O. You notice a loose PIPE that you might be able to pry off.";
+    } else if ((examineInput === "CORPSE") && !(takeArray.includes("KEYCARD")) && (tubeSmashed = true)) {
+      takeArray.push("KEYCARD");
+      return "He's dead, but he may still be useful to you. Could that be a KEYCARD sticking out of his pocket?";
+    } else if (ObjExamine.items[i] === examineInput) {
       return ObjExamine.description[i];
     }
   }//end for loop
-  return "you examined nothing";
+  return "nothing noteworthy";
 }//end examineFeature function
 
-var takeFeature = function(takeInput) {
+var takeFeature = function(takeInput) {    ///TAKE STUFF
   for (i = 0; i < takeArray.length; i++) {
     if ((takeArray[i] === takeInput) && !(inventoryArray.includes(takeArray[i]))) {
       inventoryArray.push(takeArray[i]);
       // var removeItem = takeInput;
-      // takeArray.splice( $.inArray(removeItem,takeArray) ,1 );
+      // takeArray.splice( $.inArray(removeItem,takeArray) ,1 ); //jquery remove from takeArray
       takeArray.splice(takeArray.indexOf(takeInput),1); //javascript remove from takeArray
       return "YOU TOOK SOMETHING";
     }
